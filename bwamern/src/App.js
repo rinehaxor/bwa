bce446b4a96cff68223fdf9a4b104e4044b63b54
rinehaxor <rinehaxor@users.noreply.github.com>
2022-08-10 14:26:29 +0700

    Initial commit

---
+++ bwamern/src/App.js
@@ -0,0 +1,7 @@
+import 'assets/scss/style.scss';
+
+function App() {
+  return <div className="App">hai dunia!</div>;
+}
+
+export default App;
---
+++ bwamern/src/pages/LandingPages.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import moduleName from 'elements/Button';
+
+export default class LandingPages extends Component {
+  render() {
+    return <div>LandingPages</div>;
+  }
+}
